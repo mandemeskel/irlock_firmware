bd33ac78a93c7d8c2720239c7fd768e7b9299de2
Michael T. Andemeskel <mtandemeskel@gmail.com>
2017-02-09 15:58:25 -0800

    fixed errors with building video, missing a file libfalcon_m4_spifi_drv.a in src folder

--- src/device/gcc/video/Release/objects.mk
+++ src/device/gcc/video/Release/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := 
+USER_OBJS := /home/michael/Downloads/MESA/IR_Lock_firmware/irlock_firmware/src/device/gcc/m0/Release/m0.axf.o
 
 LIBS := -lfalcon_m4_spifi_drv -lpixy_m4
 
